201a1b21e6a8b703cfc79e82e5b7aa4d70d563e1
JustSP <36706750+JustSP@users.noreply.github.com>
2020-04-21 19:37:08 +0300

    Update customThemes.dart
    Made function parameterized and scalable.

--- lib/AppThemes/CustomThemes/customThemes.dart
+++ lib/AppThemes/CustomThemes/customThemes.dart
@@ -2,12 +2,33 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class CustomAppThemes {
-  ThemeData blueTheme() {
+  ThemeData blueTheme(
+      Brightness lightOrDark, int primaryColorDegree, int accentColorDegree) {
     return ThemeData(
-        // Define the default brightness and colors.
-        brightness: Brightness.light,
-        primaryColor: Colors.blue[800],
-        accentColor: Colors.cyan[600],
-      );
+      // Define the default brightness and colors.
+      brightness: lightOrDark,
+      primaryColor: Colors.blue[primaryColorDegree],
+      accentColor: Colors.blueAccent[accentColorDegree],
+    );
+  }
+
+  ThemeData amberTheme(
+      Brightness lightOrDark, int primaryColorDegree, int accentColorDegree) {
+    return ThemeData(
+      // Define the default brightness and colors.
+      brightness: lightOrDark,
+      primaryColor: Colors.amber[primaryColorDegree],
+      accentColor: Colors.amberAccent[accentColorDegree],
+    );
+  }
+
+  ThemeData greenTheme(
+      Brightness lightOrDark, int primaryColorDegree, int accentColorDegree) {
+    return ThemeData(
+      // Define the default brightness and colors.
+      brightness: lightOrDark,
+      primaryColor: Colors.green[primaryColorDegree],
+      accentColor: Colors.greenAccent[accentColorDegree],
+    );
   }
 }
